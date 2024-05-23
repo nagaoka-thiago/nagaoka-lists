d16d36a17a4c5c2e1a09ecebaa4eaa7f3a3204ed
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2024-05-22 22:13:07 -0300

    removed stubs on unit tests that are not being used.

--- test/src/core/adapters/database/domain/usecases/add_item_usecase_test.dart
+++ test/src/core/adapters/database/domain/usecases/add_item_usecase_test.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nagaoka_lists/src/core/adapters/database/data/use_cases/add_item_usecase_impl.dart';
 import 'package:nagaoka_lists/src/core/adapters/database/domain/entities/item_entity.dart';
-import 'package:nagaoka_lists/src/core/adapters/database/domain/entities/list_entity.dart';
-import 'package:nagaoka_lists/src/core/adapters/database/domain/repositories/database_client_repository.dart';
-import 'package:nagaoka_lists/src/core/errors/error_exception.dart';
-import 'package:nagaoka_lists/src/core/generics/resource.dart';
 
 void main() async {
   group('Add Item Usecase tests', () {
@@ -33,38 +29,4 @@ void main() async {
       expect(items.length, equals(3));
     }));
   });
-}
-
-class DatabaseClientRepositoryStub implements DatabaseClientRepository {
-  late Resource<int, ErrorException> returnedUpdateDeleteAdd;
-  late Resource<ListEntity, ErrorException> returnedRead;
-  late Resource<List<ListEntity>, ErrorException> returnedReads;
-  @override
-  Future<Resource<int, ErrorException>> addList(
-      {required ListEntity list}) async {
-    return returnedUpdateDeleteAdd;
-  }
-
-  @override
-  Future<Resource<int, ErrorException>> deleteList(
-      {required String title}) async {
-    return returnedUpdateDeleteAdd;
-  }
-
-  @override
-  Future<Resource<ListEntity, ErrorException>> readList(
-      {required String title}) async {
-    return returnedRead;
-  }
-
-  @override
-  Future<Resource<List<ListEntity>, ErrorException>> readLists() async {
-    return returnedReads;
-  }
-
-  @override
-  Future<Resource<int, ErrorException>> updateList(
-      {required String title, required ListEntity list}) async {
-    return returnedUpdateDeleteAdd;
-  }
 }
--- test/src/core/adapters/database/domain/usecases/delete_item_usecase_test.dart
+++ test/src/core/adapters/database/domain/usecases/delete_item_usecase_test.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nagaoka_lists/src/core/adapters/database/data/use_cases/delete_item_usecase_impl.dart';
 import 'package:nagaoka_lists/src/core/adapters/database/domain/entities/item_entity.dart';
-import 'package:nagaoka_lists/src/core/adapters/database/domain/entities/list_entity.dart';
-import 'package:nagaoka_lists/src/core/adapters/database/domain/repositories/database_client_repository.dart';
-import 'package:nagaoka_lists/src/core/errors/error_exception.dart';
-import 'package:nagaoka_lists/src/core/generics/resource.dart';
 
 void main() async {
   group('Delete Item Usecase tests', () {
@@ -68,38 +64,4 @@ void main() async {
       expect(items.length, equals(2));
     }));
   });
-}
-
-class DatabaseClientRepositoryStub implements DatabaseClientRepository {
-  late Resource<int, ErrorException> returnedUpdateDeleteAdd;
-  late Resource<ListEntity, ErrorException> returnedRead;
-  late Resource<List<ListEntity>, ErrorException> returnedReads;
-  @override
-  Future<Resource<int, ErrorException>> addList(
-      {required ListEntity list}) async {
-    return returnedUpdateDeleteAdd;
-  }
-
-  @override
-  Future<Resource<int, ErrorException>> deleteList(
-      {required String title}) async {
-    return returnedUpdateDeleteAdd;
-  }
-
-  @override
-  Future<Resource<ListEntity, ErrorException>> readList(
-      {required String title}) async {
-    return returnedRead;
-  }
-
-  @override
-  Future<Resource<List<ListEntity>, ErrorException>> readLists() async {
-    return returnedReads;
-  }
-
-  @override
-  Future<Resource<int, ErrorException>> updateList(
-      {required String title, required ListEntity list}) async {
-    return returnedUpdateDeleteAdd;
-  }
 }
